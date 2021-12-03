36e3d495ec1827f9b90ea1e3560f0e421d9270dd
Zhenghao Li <lizhenghao99@outlook.com>
2021-12-02 22:50:43 -0800

    Image update

--- components/media/jiusi-portfolio.js
+++ components/media/jiusi-portfolio.js
@@ -33,7 +33,7 @@ const JiusiPortfolio = (props) => {
                     ratio={16 / 9}
                     src={basePath + value}
                     placeHolder={'empty'}
-                    quality={50}
+                    quality={20}
                 >
                 </ImageComponent>
             ))}
